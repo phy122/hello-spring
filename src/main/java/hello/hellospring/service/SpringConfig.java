3e41bcc9b0003b9d20463e8fc962f205b89e913b
Hoyoung Park <qkrghdud369@gmail.com>
2022-10-08 21:26:17 +0900

    SpringConfig

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    @Bean
+
     public MemberService memberService(){
         return new MemberService(memberRepository());
 
