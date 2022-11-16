6b4b6c1e5efa227520b3eef9880c2de147224c3f
Hoyoung Park <qkrghdud369@gmail.com>
2022-11-17 00:13:48 +0900

    AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,8 +4,10 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,8 +25,10 @@ public class MemberService {
     /**
      * 회원 가입
      */
-    public Long join(Member member){
+    public Long join(Member member) {
+
         //같은 이름이 있는 중복 회원X
+
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
